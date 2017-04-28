66ebc132eb8557caa2aa61cf8377717761a5b427
harshk0525 <harshk0525@gmail.com>
2017-04-28 14:40:14 +0530

    Update ultimate translator 1.0.py
    Removed languages that were not functional. Added three separate tabs for language and accent choice.

--- ultimate translator 1.0.py
+++ ultimate translator 1.0.py
@@ -1,32 +1,40 @@
-﻿from Tkinter import *
+from Tkinter import *
 from gtts import gTTS
 import os
 from translate import Translator
 
 root = Tk()
 root.title("The Ultimate Translator")
+
+var1 = StringVar(root)
+var1.set("Choose Language")
 var2 = StringVar(root)
 var2.set("Choose Language")
-drop_menu=OptionMenu(root,var2,'Afrikaans', 'Albanian', 'Arabic', 'Armenian', 'Bengali', 'Catalan', 'Chinese', 'Chinese ﴾Mandarin/China﴿', 'Chinese ﴾Mandarin/Taiwan﴿', 'Chinese ﴾Cantonese﴿', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'English ﴾Australia﴿', 'English ﴾United Kingdom﴿', 'English ﴾United States﴿', 'Esperanto', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Icelandic', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Macedonian', 'Norwegian', 'Polish', 'Portuguese', 'Portuguese ﴾Brazil﴿', 'Romanian', 'Russian', 'Serbian', 'Slovak', 'Spanish' , 'Spanish ﴾Spain﴿', 'Spanish ﴾United States﴿', 'Swahili', 'Swedish', 'Tamil', 'Thai', 'Turkish', 'Vietnamese', 'Welsh')
+var3 = StringVar(root)
+var3.set("Choose Accent")
+drop_menu=OptionMenu(root,var1,'Arabic', 'Bengali', 'Catalan', 'Chinese',  'Czech', 'Danish', 'Dutch', 'English', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian' , 'Slovak', 'Spanish' , 'Swedish', 'Thai', 'Turkish', 'Vietnamese', 'Welsh')
+drop_menu.grid(row=1,column=0, pady=15, padx=15)
+drop_menu=OptionMenu(root,var2,'Arabic', 'Bengali', 'Catalan', 'Chinese',  'Czech', 'Danish', 'Dutch', 'English', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian' , 'Slovak', 'Spanish' , 'Swedish', 'Thai', 'Turkish', 'Vietnamese', 'Welsh')
 drop_menu.grid(row=1,column=1, pady=15, padx=15)
-
+drop_menu=OptionMenu(root,var3,'Arabic', 'Bengali', 'Catalan', 'Chinese',  'Czech', 'Danish', 'Dutch', 'English', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian' , 'Slovak', 'Spanish' , 'Swedish', 'Thai', 'Turkish', 'Vietnamese', 'Welsh')
+drop_menu.grid(row=1,column=2, pady=15, padx=15)
 translation_lang_code='en'
 def translate():
     translation_lang= var2.get()
-    lang=['Afrikaans', 'Albanian', 'Arabic', 'Armenian', 'Bengali', 'Catalan', 'Chinese', 'Chinese ﴾Mandarin/China﴿', 'Chinese ﴾Mandarin/Taiwan﴿', 'Chinese ﴾Cantonese﴿', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'English ﴾Australia﴿', 'English ﴾United Kingdom﴿', 'English ﴾United States﴿', 'Esperanto', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Icelandic', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Macedonian', 'Norwegian', 'Polish', 'Portuguese', 'Portuguese ﴾Brazil﴿', 'Romanian', 'Russian', 'Serbian', 'Slovak', 'Spanish' , 'Spanish ﴾Spain﴿', 'Spanish ﴾United States﴿', 'Swahili', 'Swedish', 'Tamil', 'Thai', 'Turkish', 'Vietnamese', 'Welsh']
-    code=['af', 'sq', 'ar', 'hy', 'bn', 'ca', 'zh', 'zh‐cn', 'zh‐tw', 'zh‐yue', 'hr', 'cs', 'da', 'nl', 'en', 'en‐au', 'en‐uk', 'en‐us', 'eo', 'fi', 'fr', 'de', 'el', 'hi', 'hu', 'is', 'id', 'it', 'ja', 'ko', 'la', 'lv', 'mk', 'no', 'pl', 'pt', 'pt‐br', 'ro', 'ru', 'sr', 'sk', 'es', 'es‐es', 'es‐us', 'sw', 'sv', 'ta', 'th', 'tr', 'vi', 'cy']
-    for i in range(51):
+    lang=[ 'Arabic', 'Bengali', 'Catalan', 'Chinese',  'Czech', 'Danish', 'Dutch', 'English', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian' , 'Slovak', 'Spanish' , 'Swedish', 'Thai', 'Turkish', 'Vietnamese', 'Welsh']
+    code=['ar', 'bn', 'ca', 'zh', 'cs', 'da', 'nl', 'en', 'fi', 'fr', 'de', 'el', 'hi', 'hu', 'id', 'it', 'ja', 'ko', 'la', 'lv', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'es', 'sv', 'th', 'tr', 'vi', 'cy']
+    for i in range(32):
         if translation_lang==lang[i]:
             translation_lang_code=code[i]
     text=e1.get()
     translator= Translator(to_lang=translation_lang)
     translation = translator.translate(text)
-    return translation
+    print translation
 def speak():
-    translation_lang= var2.get()
-    lang=['Afrikaans', 'Albanian', 'Arabic', 'Armenian', 'Bengali', 'Catalan', 'Chinese', 'Chinese ﴾Mandarin/China﴿', 'Chinese ﴾Mandarin/Taiwan﴿', 'Chinese ﴾Cantonese﴿', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'English ﴾Australia﴿', 'English ﴾United Kingdom﴿', 'English ﴾United States﴿', 'Esperanto', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Icelandic', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Macedonian', 'Norwegian', 'Polish', 'Portuguese', 'Portuguese ﴾Brazil﴿', 'Romanian', 'Russian', 'Serbian', 'Slovak', 'Spanish' , 'Spanish ﴾Spain﴿', 'Spanish ﴾United States﴿', 'Swahili', 'Swedish', 'Tamil', 'Thai', 'Turkish', 'Vietnamese', 'Welsh']
-    code=['af', 'sq', 'ar', 'hy', 'bn', 'ca', 'zh', 'zh‐cn', 'zh‐tw', 'zh‐yue', 'hr', 'cs', 'da', 'nl', 'en', 'en‐au', 'en‐uk', 'en‐us', 'eo', 'fi', 'fr', 'de', 'el', 'hi', 'hu', 'is', 'id', 'it', 'ja', 'ko', 'la', 'lv', 'mk', 'no', 'pl', 'pt', 'pt‐br', 'ro', 'ru', 'sr', 'sk', 'es', 'es‐es', 'es‐us', 'sw', 'sv', 'ta', 'th', 'tr', 'vi', 'cy']
-    for i in range(51):
+    translation_lang= var1.get()
+    lang=[ 'Arabic', 'Bengali', 'Catalan', 'Chinese',  'Czech', 'Danish', 'Dutch', 'English', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian' , 'Slovak', 'Spanish' , 'Swedish', 'Thai', 'Turkish', 'Vietnamese', 'Welsh']
+    code=['ar', 'bn', 'ca', 'zh', 'cs', 'da', 'nl', 'en', 'fi', 'fr', 'de', 'el', 'hi', 'hu', 'id', 'it', 'ja', 'ko', 'la', 'lv', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'es', 'sv', 'th', 'tr', 'vi', 'cy']
+    for i in range(32):
         if translation_lang==lang[i]:
             translation_lang_code=code[i]
     text=e1.get()
@@ -37,10 +45,10 @@ def speak():
     os.system("mpg321 good.mp3")
     os.startfile('good.mp3')
 def accent():
-    translation_lang= var2.get()
-    lang=['Afrikaans', 'Albanian', 'Arabic', 'Armenian', 'Bengali', 'Catalan', 'Chinese', 'Chinese ﴾Mandarin/China﴿', 'Chinese ﴾Mandarin/Taiwan﴿', 'Chinese ﴾Cantonese﴿', 'Croatian', 'Czech', 'Danish', 'Dutch', 'English', 'English ﴾Australia﴿', 'English ﴾United Kingdom﴿', 'English ﴾United States﴿', 'Esperanto', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Icelandic', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Macedonian', 'Norwegian', 'Polish', 'Portuguese', 'Portuguese ﴾Brazil﴿', 'Romanian', 'Russian', 'Serbian', 'Slovak', 'Spanish' , 'Spanish ﴾Spain﴿', 'Spanish ﴾United States﴿', 'Swahili', 'Swedish', 'Tamil', 'Thai', 'Turkish', 'Vietnamese', 'Welsh']
-    code=['af', 'sq', 'ar', 'hy', 'bn', 'ca', 'zh', 'zh‐cn', 'zh‐tw', 'zh‐yue', 'hr', 'cs', 'da', 'nl', 'en', 'en‐au', 'en‐uk', 'en‐us', 'eo', 'fi', 'fr', 'de', 'el', 'hi', 'hu', 'is', 'id', 'it', 'ja', 'ko', 'la', 'lv', 'mk', 'no', 'pl', 'pt', 'pt‐br', 'ro', 'ru', 'sr', 'sk', 'es', 'es‐es', 'es‐us', 'sw', 'sv', 'ta', 'th', 'tr', 'vi', 'cy']
-    for i in range(51):
+    translation_lang= var3.get()
+    lang=[ 'Arabic', 'Bengali', 'Catalan', 'Chinese',  'Czech', 'Danish', 'Dutch', 'English', 'Finnish', 'French', 'German', 'Greek', 'Hindi', 'Hungarian', 'Indonesian', 'Italian', 'Japanese', 'Korean', 'Latin', 'Latvian', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Russian' , 'Slovak', 'Spanish' , 'Swedish', 'Thai', 'Turkish', 'Vietnamese', 'Welsh']
+    code=['ar', 'bn', 'ca', 'zh', 'cs', 'da', 'nl', 'en', 'fi', 'fr', 'de', 'el', 'hi', 'hu', 'id', 'it', 'ja', 'ko', 'la', 'lv', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'es', 'sv', 'th', 'tr', 'vi', 'cy']
+    for i in range(32):
         if translation_lang==lang[i]:
             translation_lang_code=code[i]
     text=e1.get()
@@ -52,11 +60,11 @@ def accent():
     
 Label(root, text="Enter Text in English").grid(row=0)
 
-e1 = Entry(root)
-e1.grid(row=0, column=1, pady=15, padx=15)
+e1 = Entry(root, width=34, bg="green")
+e1.grid(row=0, column=1, pady=15, padx=15 , columnspan = 20)
 Button(root, text='Speak in Language', command=speak).grid(row=3, column=0, pady=15, padx=15)
 Button(root, text='Translate', command=translate).grid(row=3, column=1, pady=15, padx=15)
 Button(root, text='Speak in Accent', command=accent).grid(row=3, column=2, pady=15, padx=15)
-Label(root, text="Translated Text:  "+translate()). grid(row=5, column=0, pady=15, padx=15)
+Label(root, text="Translated Text:"). grid(row=5, column=0, pady=15, padx=15)
 
 root.mainloop()
